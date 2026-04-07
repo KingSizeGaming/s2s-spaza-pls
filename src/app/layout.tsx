702a3a8a688f401b548728219d79dea48e054762
Mariane Historia <marianerosario.historia.21@usjr.edu.ph>
2026-04-07 10:48:55 +0800

    fix: button images preloaded

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preload" href="/images/exit_button_tapped.png" as="image" />
+        <link rel="preload" href="/images/no_button_tapped.png" as="image" />
+        <link rel="preload" href="/images/yes_button_tapped.png" as="image" />
+        <link rel="preload" href="/images/submit_button_tapped.png" as="image" />
+        <link rel="preload" href="/images/back_button_tapped.png" as="image" />
+        <link rel="preload" href="/images/home_button_picked.png" as="image" />
+        <link rel="preload" href="/images/draw_button_picked.png" as="image" />
+        <link rel="preload" href="/images/away_button_picked.png" as="image" />
+        <link rel="preload" href="/images/home_button_untapped.png" as="image" />
+        <link rel="preload" href="/images/draw_button_untapped.png" as="image" />
+        <link rel="preload" href="/images/away_button_untapped.png" as="image" />
+      </head>
       <body className="antialiased">
         {/* <div className="bg-amber-100 text-amber-900">
           <div className="mx-auto flex max-w-5xl items-center justify-between px-6 py-3 text-sm">
